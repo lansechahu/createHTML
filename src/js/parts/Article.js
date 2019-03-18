4e69f5888d37ae05522f609cde6048bb05be9095
lansechahu <lansechahu@126.om>
2019-03-18 19:31:08 +0800

    v1.0.5

--- src/js/parts/Article.js
+++ src/js/parts/Article.js
@@ -7,11 +7,12 @@ import * as ChcUtils from "chcutils";
 
 export default class Article {
     constructor() {
-        this.content = '<div class="area-box layui-colla-item"><div class="layui-colla-title">正文段落:</div><div class="layui-colla-content layui-show"><textarea class="txt-area" placeholder="正文段落"></textarea><div class="btnBox layui-btn-group"><button class="delBtn layui-btn layui-btn-normal" onclick="delItem(this)">删除</button><button class="moveBtn layui-btn layui-btn-normal" onclick="moveUp(this)">上移</button><button class="moveBtn layui-btn layui-btn-normal" onclick="moveDown(this)">下移</button></div><div class="clear"></div></div></div>';
+        this.content = '<div class="area-box layui-colla-item"><div class="layui-colla-title">正文段落:</div><div class="layui-colla-content layui-show"><textarea class="txt-area" placeholder="正文段落" onmouseup="areamouseup(this)"></textarea><div class="btnBox layui-btn-group"><button class="delBtn layui-btn layui-btn-normal" onclick="delItem(this)">删除</button><button class="moveBtn layui-btn layui-btn-normal" onclick="moveUp(this)">上移</button><button class="moveBtn layui-btn layui-btn-normal" onclick="moveDown(this)">下移</button></div><div class="clear"></div></div></div>';
     }
 
     add() {
         $('#edit-area').append(this.content);
+        console.log($(this.content).children('.layui-colla-content').children('.txt-area'))
         element.init('collapse');
     }
 
